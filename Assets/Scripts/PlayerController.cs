2d63f86e22dbb2035d491d7efd0327ff033b767c
MatubaraTooru <kurakura.ingen@gmail.com>
2023-06-15 05:23:12 +0900

    [add] プレイヤー追加

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rb;
+    private CinemachineFreeLook _camera;
+
     [SerializeField] private float _moveSpeed = 5.0f;
     [SerializeField] private string _merchantTag;
     [SerializeField] private GameObject _shopPanel;
+    [SerializeField] private float _sprintSpeed = 2.0f;
     private float _h;
     private float _v;
     private bool _shopping = false;
@@ -17,6 +21,7 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _camera = FindObjectOfType<CinemachineFreeLook>();
     }
 
     private void Update()
@@ -39,14 +44,20 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         Vector3 dir = Vector3.forward * _v + Vector3.right * _h;
+        dir = Camera.main.transform.TransformDirection(dir);
         dir.y = 0;
 
-        if (dir != Vector3.zero)
-        {
-            transform.forward = dir;
-        }
+        if (dir != Vector3.zero) this.transform.forward = dir;
 
-        Vector3 velocity = dir * _moveSpeed + Vector3.up * _rb.velocity.y;
-        _rb.velocity = velocity;
+        float y = _rb.velocity.y;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _rb.velocity = dir * _moveSpeed * _sprintSpeed + Vector3.up * y;
+        }
+        else
+        {
+            _rb.velocity = dir * _moveSpeed + Vector3.up * y;
+        }
     }
 }
